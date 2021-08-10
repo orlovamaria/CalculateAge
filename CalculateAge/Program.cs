255db94c886aa097f3767bbc441f65aceed594ee
orlovamaria <arlova.maryia@gmail.com>
2021-08-10 22:49:03 +0300

    Add project files.

---
+++ CalculateAge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculateAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name - ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your lastname - ");
+            string lastname = Console.ReadLine();
+            Console.Write("Enter the year of birth - ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello {name} {lastname} your age - {DateTime.Now.Year - year}");
+        }
+    }
+}
+    
